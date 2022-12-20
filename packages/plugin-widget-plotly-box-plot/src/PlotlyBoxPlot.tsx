9b6de40be688ffb31799c023ac5299e5c67008c1
Davicittod <davidhera96@gmail.com>
2022-12-20 12:55:22 +0100

    BoxPlot Implementation

--- packages/plugin-widget-plotly-box-plot/src/PlotlyBoxPlot.tsx
+++ packages/plugin-widget-plotly-box-plot/src/PlotlyBoxPlot.tsx
@@ -1,8 +1,68 @@
 import { WidgetPluginProps } from "@activeviam/activeui-sdk";
-// import useComponentSize from "@rehooks/component-size";
-import React, { FC } from "react";
-// import Plot from "react-plotly.js";
+import useComponentSize from "@rehooks/component-size";
+import React, { FC, useRef } from "react";
+import Plot from "react-plotly.js";
 
 export const PlotlyBoxPlot: FC<WidgetPluginProps> = (props) => {
-  return <div>a</div>;
+  const container = useRef<HTMLDivElement>(null);
+  const { height, width } = useComponentSize(container);
+
+  var x = [];
+  var y0 = [];
+  var y1 = [];
+  var y2 = [];
+
+  for (var i = 0; i < 50; i++) {
+    if (i < 25) {
+      x[i] = "Day 1";
+    } else {
+      x[i] = "Day 2";
+    }
+
+    y0[i] = Math.random() * 1.5;
+    y1[i] = Math.random() * 3 + 1;
+    y2[i] = Math.random() * 2 + -1;
+  }
+
+  return (
+    <div
+      ref={container}
+      style={{
+        ...props.style,
+        height: "100%",
+      }}
+    >
+      <Plot
+        data={[
+          {
+            x: x,
+            y: y0,
+            type: "box",
+            name: "Film 1",
+            marker: { color: "#3D9970" },
+          },
+          {
+            x: x,
+            y: y1,
+            type: "box",
+            name: "Film 2",
+            marker: { color: "#FF4136" },
+          },
+          {
+            x: x,
+            y: y2,
+            type: "box",
+            name: "Film 3",
+            marker: { color: "#FF851B" },
+          },
+        ]}
+        layout={{
+          showlegend: false,
+          height,
+          width,
+          boxmode: "group",
+        }}
+      />
+    </div>
+  );
 };
