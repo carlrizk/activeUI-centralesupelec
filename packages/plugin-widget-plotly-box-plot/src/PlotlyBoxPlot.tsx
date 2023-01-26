051eda2323a8cccfc762e84ab9a1d9432d49f9c0
Davicittod <davidhera96@gmail.com>
2023-01-26 10:20:22 +0100

    Refactorization with extractData and upgrade of ActiveViam to v.5.0.20

--- packages/plugin-widget-plotly-box-plot/src/PlotlyBoxPlot.tsx
+++ packages/plugin-widget-plotly-box-plot/src/PlotlyBoxPlot.tsx
@@ -1,85 +1,24 @@
 import { PlotBase } from "@activeui-cs/react-utils";
-import { CellSet, withQueryResult } from "@activeviam/activeui-sdk";
+import { withQueryResult } from "@activeviam/activeui-sdk";
 import useComponentSize from "@rehooks/component-size";
 import React, { useRef } from "react";
 import Spin from "antd/lib/spin";
 import { PlotlyBoxPlotWidgetState } from "./PlotlyBoxPlot.types";
-import { PlotlyBoxPlotData } from "./PlotlyBoxPlotData";
+import { extractData } from "@activeui-cs/common";
 
 export const PlotlyBoxPlot = withQueryResult<PlotlyBoxPlotWidgetState>(
   (props) => {
     const { data, error, isLoading } = props.queryResult;
 
-    const extractData = (data?: CellSet): PlotlyBoxPlotData[] => {
-      if (data == null) return [];
-      const result: PlotlyBoxPlotData[] = [];
-
-      const columnsAxis = data.axes[0];
-      const measures = columnsAxis.positions.map(
-        (position) => position[0].captionPath[0]
-      );
-      // console.log("Labels: ", measures);
-
-      if (measures.length === 0) return [];
-      console.log("Data inside: ", data);
-
-      const values: number[][] = new Array(measures.length);
-      for (let i = 0; i < values.length; i++) {
-        values[i] = [];
-      }
-
-      // console.log("Values: ", values);
-
-      for (let i = measures.length; i < data.cells.length; i++) {
-        values[i % measures.length].push(data.cells[i].value as number);
-      }
-
-      for (let i = 0; i < measures.length; i++) {
-        result.push({
-          name: measures[i],
-          values: values[i],
-        });
-      }
-
-      return result;
-    };
-
     const extractedData = extractData(data);
-    const plotData: Plotly.Data[] = extractedData.map((result) => {
-      return { y: result.values, type: "box", name: result.name };
-    });
 
-    // const extractData = (data?: CellSet): any => {
-    //   console.log("Extracted data: ", data)
-    // }
-    console.log("Extracted data: ", extractedData);
+    // Possibility to add functionalities
+    const plotData: Plotly.Data[] = extractedData.map((result) => {
+      return { y: result.values, type: "box", name: result.measureName };
+    });
 
     const container = useRef<HTMLDivElement>(null);
     const { height, width } = useComponentSize(container);
-
-    // const oldData = [
-    //   {
-    //     x,
-    //     y: y0,
-    //     type: "box",
-    //     name: "Film 1",
-    //     marker: { color: "#3D9970" },
-    //   },
-    //   {
-    //     x,
-    //     y: y1,
-    //     type: "box",
-    //     name: "Film 2",
-    //     marker: { color: "#FF4136" },
-    //   },
-    //   {
-    //     x,
-    //     y: y2,
-    //     type: "box",
-    //     name: "Film 3",
-    //     marker: { color: "#FF851B" },
-    //   },
-    // ];
 
     return (
       <div
--- packages/plugin-widget-plotly-box-plot/src/PlotlyBoxPlotData.tsx
+++
@@ -1,4 +0,0 @@
-export interface PlotlyBoxPlotData {
-  name: string;
-  values: number[];
-}
