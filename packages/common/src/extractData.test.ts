6d5a9c85590ead4777d4d2f4b235dc01eb026068
Davicittod <davihera98@gmail.com>
2023-03-08 16:10:54 +0100

    Modifications to improve readability

--- packages/common/src/extractData.test.ts
+++ packages/common/src/extractData.test.ts
@@ -1,14 +1,29 @@
 import { CellSet, Axis, Cell } from "@activeviam/activeui-sdk";
 import { extractData } from "./extractData.js";
 
+/*
+Transforms a vector of measures into a vector of measures with the Cell type
+*/
+function convertToCellFormat(measureValues: number[]): Cell[] {
+  return measureValues.map((value, index) => {
+    const aux: Cell = {
+      formattedValue: value.toString(),
+      ordinal: index,
+      value,
+      properties: {},
+    };
+    return aux;
+  });
+}
+
 describe(extractData, () => {
   describe("should return an empty list", () => {
-    test("If data is undefined", () => {
+    test("if data is undefined", () => {
       const data: CellSet | undefined = undefined;
       expect(extractData(data)).toStrictEqual([]);
     });
 
-    test("If data is missing an axis", () => {
+    test("if data is missing an axis", () => {
       const data: CellSet = {
         axes: [],
         cells: [],
@@ -19,24 +34,25 @@ describe(extractData, () => {
       expect(extractData(data)).toStrictEqual([]);
     });
   });
+
   describe("should not return an empty list", () => {
-    test("with an unitary element (not a list)", () => {
-      const ax1: Axis = {
+    test("with only one measure at the same time", () => {
+      // Construction of both axis row and column with the necessary parameters
+      const row: Axis = {
         id: 0,
         hierarchies: [],
         positions: [
           [
             {
-              captionPath: ["testingData"],
-              namePath: ["testingData"],
+              captionPath: ["measureData"],
+              namePath: ["measureData"],
               properties: {},
             },
           ],
         ],
         maxLevelPerHierarchy: [1],
       };
-
-      const ax2: Axis = {
+      const col: Axis = {
         id: 1,
         hierarchies: [],
         positions: [
@@ -51,55 +67,51 @@ describe(extractData, () => {
         maxLevelPerHierarchy: [2],
       };
 
-      const cells = [50, 10, 10, 20, -10, 15, 5].map((value, index) => {
-        const aux: Cell = {
-          formattedValue: value.toString(),
-          ordinal: index,
-          value,
-          properties: {},
-        };
-        return aux;
-      });
+      // Creation of the measure values with the correct format
+      const valuesUsed = [50, 10, 10, 20, -10, 15, 5];
+      const cells = convertToCellFormat(valuesUsed);
 
+      // Our function extractData expects a CellSet type to work correctly
       const data: CellSet = {
-        axes: [ax1, ax2],
+        axes: [row, col],
         cells,
         cube: "nameTest",
         epoch: 1234,
         defaultMembers: [],
       };
+
       expect(extractData(data)).toStrictEqual([
         {
-          measureName: "testingData",
+          measureName: "measureData",
           sum: 50,
           values: [10, 10, 20, -10, 15, 5],
         },
       ]);
     });
-    test("with a list", () => {
-      const ax1: Axis = {
+
+    test("with multiple measures at the same time", () => {
+      const row: Axis = {
         id: 0,
         hierarchies: [],
         positions: [
           [
             {
-              captionPath: ["testingData1"],
-              namePath: ["testingData1"],
+              captionPath: ["measureData1"],
+              namePath: ["measureData1"],
               properties: {},
             },
           ],
           [
             {
-              captionPath: ["testingData2"],
-              namePath: ["testingData2"],
+              captionPath: ["measureData2"],
+              namePath: ["measureData2"],
               properties: {},
             },
           ],
         ],
         maxLevelPerHierarchy: [1],
       };
-
-      const ax2: Axis = {
+      const col: Axis = {
         id: 1,
         hierarchies: [],
         positions: [
@@ -116,24 +128,19 @@ describe(extractData, () => {
 
       const measure1 = [50, 30, 10, 10, 20, -10, -10];
       const measure2 = [30, 10, 10, 40, -35, 10, -5];
+      /*
+      The mdx format outputs the measure values alternating between each measure included
+      We have thereby to intercalate the values of each measure
+      */
       const mixmeasure: number[] = measure1
         .map((element, index) => {
           return [element, measure2[index]];
         })
         .flat();
-
-      const cells = mixmeasure.map((value, index) => {
-        const aux: Cell = {
-          formattedValue: value.toString(),
-          ordinal: index,
-          value,
-          properties: {},
-        };
-        return aux;
-      });
+      const cells = convertToCellFormat(mixmeasure);
 
       const data: CellSet = {
-        axes: [ax1, ax2],
+        axes: [row, col],
         cells,
         cube: "nameTest",
         epoch: 1234,
@@ -142,12 +149,12 @@ describe(extractData, () => {
 
       expect(extractData(data)).toStrictEqual([
         {
-          measureName: "testingData1",
+          measureName: "measureData1",
           sum: 50,
           values: [30, 10, 10, 20, -10, -10],
         },
         {
-          measureName: "testingData2",
+          measureName: "measureData2",
           sum: 30,
           values: [10, 10, 40, -35, 10, -5],
         },
